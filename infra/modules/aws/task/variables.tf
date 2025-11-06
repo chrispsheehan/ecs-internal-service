10277e16513511a2ed4fbd317eed0df612365a00
chrispsheehan <chrispsheehan@gmail.com>
2025-11-06 14:48:40 +0000

    chore: init infra for image and task

---
+++ infra/modules/aws/task/variables.tf
@@ -0,0 +1,21 @@
+variable "project_name" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "container_port" {
+  type = number
+}
+
+variable "cpu" {
+  type    = number
+  default = 256
+}
+
+variable "memory" {
+  type    = number
+  default = 512
+}
