cef74f0f7f222dcb9cee05b2793faac182596e51
chrispsheehan <chrispsheehan@gmail.com>
2025-11-11 11:29:39 +0000

    chore: add rout table data{}

--- infra/modules/aws/ecs/data.tf
+++ infra/modules/aws/ecs/data.tf
@@ -16,3 +16,10 @@ data "aws_subnets" "private" {
     values = ["*private*"]
   }
 }
+
+data "aws_route_tables" "subnet_route_tables" {
+  filter {
+    name   = "association.subnet-id"
+    values = data.aws_subnets.private.ids
+  }
+}
