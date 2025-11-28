33de2e031f0a35d0780159e84f6cfbcc233a3b83
chrispsheehan <chrispsheehan@gmail.com>
2025-11-28 16:30:39 +0000

    fix: allow ecs to check lb tg health

--- infra/modules/aws/task/data.tf
+++ infra/modules/aws/task/data.tf
@@ -95,3 +95,14 @@ data "aws_iam_policy_document" "s3_list" {
     resources = ["*"]
   }
 }
+
+data "aws_iam_policy_document" "lb_validation" {
+  statement {
+    actions = [
+      "elasticloadbalancing:DescribeTargetGroups",
+      "elasticloadbalancing:DescribeTargetHealth"
+    ]
+    effect    = "Allow"
+    resources = ["*"]
+  }
+}
