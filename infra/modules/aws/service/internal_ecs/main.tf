2e63efd6183551926fbcbe370a68584bad49553d
Chris Sheehan <chris_sheehan@hotmail.co.uk>
2025-11-29 13:51:05 +0000

    chore: rm ecs svc roles

--- infra/modules/aws/service/internal_ecs/main.tf
+++ infra/modules/aws/service/internal_ecs/main.tf
@@ -1,27 +1,9 @@
-resource "aws_iam_role" "ecs_service_role" {
-  name               = "${var.service_name}-ecs-service-role"
-  description        = "Role used by ECS service for load balancer management"
-  assume_role_policy = data.aws_iam_policy_document.ecs_service_assume_role.json
-}
-
-resource "aws_iam_policy" "ecs_service_policy" {
-  name   = "${var.service_name}-ecs-service-policy"
-  policy = data.aws_iam_policy_document.ecs_service_policy.json
-}
-
-resource "aws_iam_role_policy_attachment" "ecs_service_policy_attachment" {
-  role       = aws_iam_role.ecs_service_role.name
-  policy_arn = aws_iam_policy.ecs_service_policy.arn
-}
-
 resource "aws_ecs_service" "service" {
   name            = var.service_name
   cluster         = data.aws_ecs_cluster.this.id
   task_definition = var.task_definition_arn
   desired_count   = var.desired_task_count
   launch_type     = "FARGATE"
-
-  iam_role = aws_iam_role.ecs_service_role.arn
 
   network_configuration {
     subnets          = var.private_subnet_ids
