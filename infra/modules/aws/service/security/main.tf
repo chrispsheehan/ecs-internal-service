896e84bfab2c0f9e6b1d164ddbf5efd891c07890
Chris Sheehan <chris_sheehan@hotmail.co.uk>
2025-11-30 19:26:21 +0000

    chore: sg rule block

--- infra/modules/aws/service/security/main.tf
+++ infra/modules/aws/service/security/main.tf
@@ -1,14 +1,15 @@
 resource "aws_security_group" "vpc_link_sg" {
   name_prefix = "${var.service_name}-vpc-link"
   vpc_id      = var.vpc_id
+}
 
-  egress {
-    description = "VPC Link to NLB"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_security_group_rule" "vpc_link_to_nlb" {
+  type                     = "egress"
+  from_port                = 80
+  to_port                  = 80
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.vpc_link_sg.id
+  source_security_group_id = aws_security_group.lb_sg.id
 }
 
 resource "aws_security_group" "lb_sg" {
