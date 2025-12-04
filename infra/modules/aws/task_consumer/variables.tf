7855277f450f764fde0a7dd4e4a54070a843a52d
chrispsheehan <chrispsheehan@gmail.com>
2025-12-04 15:45:01 +0000

    fix: add state required vars

--- infra/modules/aws/task_consumer/variables.tf
+++ infra/modules/aws/task_consumer/variables.tf
@@ -1,3 +1,15 @@
+variable "state_bucket" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "aws_region" {
+  type = string
+}
+
 variable "project_name" {
   type = string
 }
