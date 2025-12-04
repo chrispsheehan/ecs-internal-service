e7c8f2f2f05dcb07aef3589d0b9f6a5e198dadf3
chrispsheehan <chrispsheehan@gmail.com>
2025-12-04 13:19:17 +0000

    chore: sqs separate read and write policies

--- infra/modules/aws/_shared/sqs/data.tf
+++ infra/modules/aws/_shared/sqs/data.tf
@@ -1,4 +1,4 @@
-data "aws_iam_policy_document" "sqs_read_write" {
+data "aws_iam_policy_document" "sqs_read" {
   statement {
     effect = "Allow"
     actions = [
@@ -6,6 +6,18 @@ data "aws_iam_policy_document" "sqs_read_write" {
       "sqs:DeleteMessage",
       "sqs:GetQueueAttributes",
       "sqs:ChangeMessageVisibility"
+    ]
+    resources = [aws_sqs_queue.queue.arn]
+  }
+}
+
+data "aws_iam_policy_document" "sqs_write" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "sqs:SendMessage",
+      "sqs:SendMessageBatch",
+      "sqs:GetQueueAttributes"
     ]
     resources = [aws_sqs_queue.queue.arn]
   }
--- infra/modules/aws/_shared/sqs/main.tf
+++ infra/modules/aws/_shared/sqs/main.tf
@@ -5,7 +5,12 @@ resource "aws_sqs_queue" "queue" {
   receive_wait_time_seconds = 10
 }
 
-resource "aws_iam_policy" "queue_read_write_policy" {
-  name   = "${var.sqs_queue_name}-sqs-read-write-policy"
-  policy = data.aws_iam_policy_document.sqs_read_write.json
+resource "aws_iam_policy" "queue_read_policy" {
+  name   = "${var.sqs_queue_name}-sqs-read-policy"
+  policy = data.aws_iam_policy_document.sqs_read.json
+}
+
+resource "aws_iam_policy" "queue_write_policy" {
+  name   = "${var.sqs_queue_name}-sqs-write-policy"
+  policy = data.aws_iam_policy_document.sqs_write.json
 }
